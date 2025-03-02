85ebe5db0ec9825d34285adb7eb31508c5bde034
Wong Nicholas <wongyiznicholas@gmail.com>
2025-03-02 09:25:24 +0800

    feat: remove v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
 "use client";
 
-import Button from "./components/button";
-import Category from "./components/category";
-import Profile from "./components/profile";
-
 export default function Home() {
   return (
     <main className="p-4 sm:p-10 bg-gray-100">
-      <div className="max-w-[1440px] flex justify-self-center flex-col">
-        <Profile />
-        <Category />
-      </div>
+      <div className="max-w-[1440px] flex justify-self-center flex-col"></div>
     </main>
   );
 }
